73df9aa2a7dfe536e00f77d4e8c8399cefd0ad3c
LutfiSyahindra <syahindrafi@gmail.com>
2025-08-07 10:55:42 +0700

    footer

--- resources/views/template/partials/footer.blade.php
+++ resources/views/template/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer
     class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
     <p class="text-muted mb-1 mb-md-0">Copyright © 2025 <a href="https://www.sisney.my.id"
-            target="_blank">LutfiSyahindra</a>.</p>
+            target="_blank">LutfiSyahindraa</a>.</p>
     <p class="text-muted">Handcrafted With <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
 </footer>
